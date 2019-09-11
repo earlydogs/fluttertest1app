18648c049ab942fcd01d3ff0eebea7e1cc2bf83e
Lovina developer <masafumi.m@mmlovina.com>
2019-09-11 21:40:23 +0900

    20190911 1st StatefullWidget

--- lib/main.dart
+++ lib/main.dart
@@ -3,19 +3,66 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+
+
+  final title = 'iCC 複利計算';
+
   @override
   Widget build(BuildContext context) {
+
     return new MaterialApp(
       title: 'Flutter Demo',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('iCC 複利計算'),
-        ),
-        body: Text(
-          'hello, Flutter World!!',
-          style: TextStyle(fontSize: 32.0),
-        ),
+      home: new MyHomePage(
+          title: this.title,
+      ),
+    );
+  }
+}
+class MyHomePage extends StatefulWidget{
+
+
+  MyHomePage({this.title}): super();
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+
+
+}
+
+class _MyHomePageState extends State<MyHomePage>{
+
+  String _message;
+
+  @override
+  void initState(){
+    super.initState();
+    _message = 'Hello! Flutter!';
+  }
+
+  void _setMessage(){
+    setState(() {
+      _message = 'Yeah!!';
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+
+      body: Text(
+        _message,
+        style: TextStyle(fontSize: 32.0),
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        onPressed: _setMessage,
+        tooltip: 'set message',
+        child: Icon(Icons.star),
       ),
 
     );
