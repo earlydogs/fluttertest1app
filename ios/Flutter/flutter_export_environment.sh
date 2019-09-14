879b3ae1b83c1e8eb66cf94d51894e1a76efa27d
Lovina developer <masafumi.m@mmlovina.com>
2019-09-14 18:49:20 +0900

    20190914 1st Text field yeah

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/mmlovina/development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/mmlovina/AndroidStudioProjects/flutter_test1app"
+export "FLUTTER_TARGET=/Users/mmlovina/AndroidStudioProjects/flutter_test1app/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/mmlovina/development/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "TRACK_WIDGET_CREATION=true"
--- lib/main.dart
+++ lib/main.dart
@@ -1,88 +1,151 @@
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(new MyApp());
+}
 
 class MyApp extends StatelessWidget {
-
-
-  final title = 'iCC 複利計算';
-
   @override
   Widget build(BuildContext context) {
-
     return new MaterialApp(
-      title: 'Flutter Demo',
-      home: new MyHomePage(
-          title: this.title,
+      title: 'Generated App',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+        primaryColor: const Color(0xFF2196f3),
+        accentColor: const Color(0xFF2196f3),
+        canvasColor: const Color(0xFFfafafa),
       ),
+      home: new MyHomePage(),
     );
   }
 }
-class MyHomePage extends StatefulWidget{
 
-
-  MyHomePage({this.title}): super();
-
-  final String title;
-
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key}) : super(key: key);
   @override
   _MyHomePageState createState() => new _MyHomePageState();
-
-
 }
 
-class Data{
-  int _price;
-  String _name;
-
-  Data(this._name, this._price): super();
-  @override
-  String toString() {
-    // TODO: implement toString
-    return _name + ':' + _price.toString() + '円';
-  }
-}
-
-class _MyHomePageState extends State<MyHomePage>{
-
-  static final _data = [
-    Data('Apple',200),
-    Data('Orange',150),
-    Data('Peach',300)
-  ];
-  Data _item;
+class _MyHomePageState extends State<MyHomePage> {
+  var _message;
+  final controller = TextEditingController();
+  final controllerTwo = TextEditingController();
 
   @override
-  void initState(){
+  void initState() {
+    _message = 'OK';
     super.initState();
-    _item = _data[0];
-  }
-
-  void _setData(){
-    setState(() {
-      _item = (_data..shuffle()).first;
-    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('App Name'),
       ),
+      body: new GridView.extent(
+        maxCrossAxisExtent: 450.0,
+        mainAxisSpacing: 10.0,
+        crossAxisSpacing: 10.0,
+        padding: const EdgeInsets.all(10.0),
+        children: <Widget>[
+          Container(
+              color: Colors.white,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                mainAxisSize: MainAxisSize.max,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.all(20.0),
+                    child: Text(
+                      _message,
+                      style: TextStyle(
+                          fontSize: 28.0,
+                          color: Colors.pink,
+                          fontWeight: FontWeight.w400,
+                          fontFamily: "Roboto"),
+                    ),
+                  ),
 
-      body: Text(
-        _item.toString(),
-        style: TextStyle(fontSize: 32.0),
+                  Padding(
+                    padding: EdgeInsets.all(5.0),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        labelText: "元本（万円）",
+                        hintText: "100",
+                        icon: Icon(Icons.person_outline),
+                        fillColor: Colors.grey,
+                      ),
+                      controller: controller,
+                      keyboardType: TextInputType.number,
+                      style: TextStyle(
+                          fontSize: 24.0,
+                          color: Colors.pink,
+                          fontWeight: FontWeight.w400,
+                          fontFamily: "Roboto"),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.all(5.0),
+                    child: TextField(
+                      decoration: InputDecoration(
+                        labelText: "積立金額（万円）",
+                        hintText: "5",
+                        icon: Icon(Icons.person_outline),
+                        fillColor: Colors.blue,
+                      ),
+                      controller: controllerTwo,
+                      keyboardType: TextInputType.number,
+                      style: TextStyle(
+                          fontSize: 24.0,
+                          color: Colors.pink,
+                          fontWeight: FontWeight.w400,
+                          fontFamily: "Roboto"),
+                    ),
+                  ),
+                  FlatButton(
+                    padding: EdgeInsets.all(10.0),
+                    color: Colors.pink,
+                    child: Text(
+                      "push here",
+                      style: TextStyle(
+                          fontSize: 32.0,
+                          fontWeight: FontWeight.w400,
+                          fontFamily: "Roboto"),
+                    ),
+                    onPressed: buttonPressed,
+                  ),
+                ],
+              )),
+          Container(
+            color: Colors.green,
+            child: Text(
+              "Two",
+              style: TextStyle(
+                  fontSize: 32.0,
+                  fontWeight: FontWeight.w400,
+                  fontFamily: "Roboto"),
+            ),
+          ),
+          Container(
+            color: Colors.blue,
+            child: Text(
+              "Three",
+              style: TextStyle(
+                  fontSize: 32.0,
+                  fontWeight: FontWeight.w400,
+                  fontFamily: "Roboto"),
+            ),
+          ),
+        ],
       ),
-
-      floatingActionButton: FloatingActionButton(
-        onPressed: _setData,
-        tooltip: 'set message',
-        child: Icon(Icons.star),
-      ),
-
     );
+  }
+
+  void buttonPressed() {
+    setState(() {
+      _message = controller.text;
+    });
   }
 }
