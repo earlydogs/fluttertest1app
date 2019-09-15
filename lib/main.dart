c9416f40af4017d45e65c51db91bfd0c93f6c456
Lovina developer <masafumi.m@mmlovina.com>
2019-09-15 16:38:00 +0900

    20190915 1st Input field

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:decimal/decimal.dart';
 
 void main() {
   runApp(new MyApp());
@@ -27,9 +28,17 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  //とりあえずやってみよう。ウィジェットのビルドは最後に記載するとわかりやすい。
+
   var _message;
-  final controller = TextEditingController();
-  final controllerTwo = TextEditingController();
+
+  //入力項目のコントローラ
+  final controllerCurrentBalance   = TextEditingController();
+  final controllerMonthlyAddition  = TextEditingController();
+  final controllerInterestRateYear = TextEditingController();
+  final controllerPeriodYear       = TextEditingController();
+
+
 
   @override
   void initState() {
@@ -37,25 +46,149 @@ class _MyHomePageState extends State<MyHomePage> {
     super.initState();
   }
 
+  void buttonPressed() {
+    //big decimal で受け取り
+    var inputCurrentBalance   = Decimal.parse(controllerCurrentBalance.text);
+    var inputMonthlyAddition  = Decimal.parse(controllerMonthlyAddition.text);
+    var inputInterestRateYear = Decimal.parse(controllerInterestRateYear.text);
+    var inputPeriodYear       = Decimal.parse(controllerPeriodYear.text);
+
+
+    setState(() {
+      _message = controllerCurrentBalance.text;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text('App Name'),
+        title: new Text('iCC 複利計算App',
+            style: TextStyle(
+            fontSize: 24.0,
+            color: Colors.white,
+            fontWeight: FontWeight.w400,
+            fontFamily: "Roboto")),
       ),
       body: new GridView.extent(
-        maxCrossAxisExtent: 450.0,
+        maxCrossAxisExtent: 600.0,
         mainAxisSpacing: 10.0,
         crossAxisSpacing: 10.0,
+        childAspectRatio: 0.7,
         padding: const EdgeInsets.all(10.0),
         children: <Widget>[
           Container(
-              color: Colors.white,
+              color: Colors.white70,
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.start,
                 mainAxisSize: MainAxisSize.max,
                 crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: <Widget>[
+
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(20.0, 10.0, 25.0, 5.0),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        labelText: "元本（万円）",
+                        hintText: "数値で入力してください",
+                        icon: Icon(Icons.attach_money),
+                        fillColor: Colors.blueAccent,
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0)),
+                      ),
+                      controller: controllerCurrentBalance,
+                      keyboardType: TextInputType.number,
+                      style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.blueAccent,
+                          fontWeight: FontWeight.w400,
+                          fontFamily: "Roboto"),
+                    ),
+                  ),
+
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(20.0, 5.0, 25.0, 5.0),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        labelText: "積立金額（万円）",
+                        hintText: "数値で入力してください",
+                        icon: Icon(Icons.add_circle_outline),
+                        fillColor: Colors.blueAccent,
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0)),
+                      ),
+                      controller: controllerMonthlyAddition,
+                      keyboardType: TextInputType.number,
+                      style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.blueAccent,
+                          fontWeight: FontWeight.w400,
+                          fontFamily: "Roboto"),
+                    ),
+                  ),
+
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(20.0, 5.0, 25.0, 5.0),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        labelText: "年利（％）",
+                        hintText: "数値で入力してください",
+                        icon: Icon(Icons.cached),
+                        fillColor: Colors.blueAccent,
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0)),
+                      ),
+                      controller: controllerInterestRateYear,
+                      keyboardType: TextInputType.number,
+                      style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.blueAccent,
+                          fontWeight: FontWeight.w400,
+                          fontFamily: "Roboto"),
+                    ),
+                  ),
+
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(20.0, 5.0, 25.0, 5.0),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        labelText: "投資期間（年）",
+                        hintText: "数値で入力してください",
+                        icon: Icon(Icons.schedule),
+                        fillColor: Colors.blueAccent,
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0)),
+                      ),
+                      controller: controllerPeriodYear,
+                      keyboardType: TextInputType.number,
+                      style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.blueAccent,
+                          fontWeight: FontWeight.w400,
+                          fontFamily: "Roboto"),
+                    ),
+                  ),
+
+                  Padding(
+                    padding: EdgeInsets.fromLTRB(25.0, 5.0, 25.0, 5.0),
+                    child: FlatButton(
+                      padding: EdgeInsets.all(15.0),
+                      color: Colors.blueAccent,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                      ),
+                      child: Text(
+                        "複利計算",
+                        style: TextStyle(
+                            fontSize: 24.0,
+                            fontWeight: FontWeight.w400,
+                            fontFamily: "Roboto",
+                            color: Colors.white),
+                      ),
+                      onPressed: buttonPressed,
+                    ),
+                  ),
+
                   Padding(
                     padding: EdgeInsets.all(20.0),
                     child: Text(
@@ -66,55 +199,6 @@ class _MyHomePageState extends State<MyHomePage> {
                           fontWeight: FontWeight.w400,
                           fontFamily: "Roboto"),
                     ),
-                  ),
-
-                  Padding(
-                    padding: EdgeInsets.all(5.0),
-                    child: TextField(
-                      decoration: InputDecoration(
-                        labelText: "元本（万円）",
-                        hintText: "100",
-                        icon: Icon(Icons.person_outline),
-                        fillColor: Colors.grey,
-                      ),
-                      controller: controller,
-                      keyboardType: TextInputType.number,
-                      style: TextStyle(
-                          fontSize: 24.0,
-                          color: Colors.pink,
-                          fontWeight: FontWeight.w400,
-                          fontFamily: "Roboto"),
-                    ),
-                  ),
-                  Padding(
-                    padding: EdgeInsets.all(5.0),
-                    child: TextField(
-                      decoration: InputDecoration(
-                        labelText: "積立金額（万円）",
-                        hintText: "5",
-                        icon: Icon(Icons.person_outline),
-                        fillColor: Colors.blue,
-                      ),
-                      controller: controllerTwo,
-                      keyboardType: TextInputType.number,
-                      style: TextStyle(
-                          fontSize: 24.0,
-                          color: Colors.pink,
-                          fontWeight: FontWeight.w400,
-                          fontFamily: "Roboto"),
-                    ),
-                  ),
-                  FlatButton(
-                    padding: EdgeInsets.all(10.0),
-                    color: Colors.pink,
-                    child: Text(
-                      "push here",
-                      style: TextStyle(
-                          fontSize: 32.0,
-                          fontWeight: FontWeight.w400,
-                          fontFamily: "Roboto"),
-                    ),
-                    onPressed: buttonPressed,
                   ),
                 ],
               )),
@@ -141,11 +225,5 @@ class _MyHomePageState extends State<MyHomePage> {
         ],
       ),
     );
-  }
-
-  void buttonPressed() {
-    setState(() {
-      _message = controller.text;
-    });
   }
 }
